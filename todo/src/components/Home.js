a84973abf8c93efb1219833b97a7bd522895346a
shaniefrater <shaniemarie@live.co.uk>
2021-02-08 09:54:51 +0000

    css style editing

--- todo/src/components/Home.js
+++ todo/src/components/Home.js
@@ -4,9 +4,9 @@ import background from "./background.png";
 function Home () {
 	return (
 		<div className="container">
-			<h1 className="welcome"> Welcome to SIJ - Task Manager</h1>
-			<h2 className="login"> Login / User Access </h2>
-			<h3 className="newUser"> New here? Sign up for an account </h3>
+		{/* <h1 className="welcome"> Welcome to DCP - Task Manager</h1> */}
+		{/* <h2 className="login"> Login / User Access </h2> */}
+		{/* <h3 className="newUser"> New here? Sign up for an account </h3> */}
 		</div>
 	)
 }
--- todo/src/components/Userprofile.js
+++ todo/src/components/Userprofile.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import Navbar from './Navbar'
 
+// function Userprofile() {
+//     return (
+//         <div className="userProfile">
+//             <a href=""></a>
+//             <ul className="userContainer">
+//                 <li className="userDetails"> Name:  </li>
+//                 <li className="userDetails"> Position: Assistant Administrator</li>
+//                 <li className="userDetails"> Earliest Task Due Date: 28/02/2020</li>
+//                 <li className="userDetails"> Number of projects: 1 </li>
+//                 <li className="userDetails"> Manager: Cleo Longsmith </li>
+//             </ul>
+//         </div>
+//     )
+// }
+
 function Userprofile() {
     return (
-        <div className="userProfile">
-            <a href=""></a>
-            <ul className="userContainer">
-                <li className="userDetails"> Name:  </li>
-                <li className="userDetails"> Position: Assistant Administrator</li>
-                <li className="userDetails"> Earliest Task Due Date: 28/02/2020</li>
-                <li className="userDetails"> Number of projects: 1 </li>
-                <li className="userDetails"> Manager: Cleo Longsmith </li>
-            </ul>
+        <div className="box">
+            <img src="https://www.w3schools.com/w3images/avatar2.png" className="img" href="Office Avatar"></img>
+            <label className="button" for="file">Edit Avatar</label>
+            <input className="button" type="file" name="" id="file" accept="image"></input>
+            <input className="button" type="text" name="" placeholder="Username"></input>
+            <input className="button" type="email" name="" placeholder="Email ID"></input>
+            <input className="button" type="text" name="" placeholder="Managed by:"></input>
+            <input className="button" type="text" name="" placeholder="Gender"></input>
+            <button className="button1"> Save changes </button>
+            <button className="button1"> Cancel changes </button>
         </div>
     )
 }
-
-
 
 export default Userprofile;
